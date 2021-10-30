153aa91e0dcdc39a3d8a21e71e9fd952627eb0a0
Ray <balafranzraymond@gmail.com>
2021-10-30 22:11:31 +0800

    fix: added Prettier + ESlinting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { BiCalendar } from 'react-icons/bi'
-import AppointmentInfo from './components/AppointmentInfo'
-import AddAppointment from './components/AddAppointment'
-import Search from './components/Search'
-import appointmentList from './data.json'
+import { BiCalendar } from "react-icons/bi";
+import AppointmentInfo from "./components/AppointmentInfo";
+import AddAppointment from "./components/AddAppointment";
+import Search from "./components/Search";
+import appointmentList from "./data.json";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
-        <BiCalendar className="inline-block text-red-400 align-top"/> Your Appointments </h1>
-        <AddAppointment />
-        <Search />
+        <BiCalendar className="inline-block text-red-400 align-top" /> Your
+        Appointments{" "}
+      </h1>
+      <AddAppointment />
+      <Search />
 
-        <ul className="divide-y divide-gray-200">
-          {appointmentList
-            .map(appointment => (
-              <AppointmentInfo key={appointment.id}
-                appointment={appointment}
-              />
-            ))
-          }
-        </ul>
+      <ul className="divide-y divide-gray-200">
+        {appointmentList.map((appointment) => (
+          <AppointmentInfo key={appointment.id} appointment={appointment} />
+        ))}
+      </ul>
     </div>
   );
 }
