6da8127369581ce0cf63e3a000d66f227a35c34c
Ray <balafranzraymond@gmail.com>
2021-10-30 21:48:07 +0800

    feature: added AppointmentInfo component to pass json data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BiCalendar } from 'react-icons/bi'
+import AppointmentInfo from './components/AppointmentInfo'
 import AddAppointment from './components/AddAppointment'
 import Search from './components/Search'
+import appointmentList from './data.json'
 
 function App() {
   return (
@@ -9,6 +11,16 @@ function App() {
         <BiCalendar className="inline-block text-red-400 align-top"/> Your Appointments </h1>
         <AddAppointment />
         <Search />
+
+        <ul className="divide-y divide-gray-200">
+          {appointmentList
+            .map(appointment => (
+              <AppointmentInfo key={appointment.id}
+                appointment={appointment}
+              />
+            ))
+          }
+        </ul>
     </div>
   );
 }
